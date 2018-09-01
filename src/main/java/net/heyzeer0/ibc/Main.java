2a6dc4eab9787b591e2db7d9425838f9e05c7374
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-09-01 19:26:00 -0300

    Initial commit

---
+++ src/main/java/net/heyzeer0/ibc/Main.java
@@ -0,0 +1,13 @@
+package net.heyzeer0.ibc;
+
+/**
+ * Created by HeyZeer0 on 01/09/2018.
+ * Copyright © HeyZeer0 - 2016
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
